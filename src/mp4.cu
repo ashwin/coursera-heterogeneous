df749693800745bf99e6554147aac885a5e6ea2b
André Orr <andre_orr@yahoo.com>
2015-01-25 15:36:10 +1100

    Update mp4 source file

--- src/mp4.cu
+++ src/mp4.cu
@@ -1,93 +1,103 @@
-// MP 4 Reduction
-// Given a list (lst) of length n
-// Output its sum = lst[0] + lst[1] + ... + lst[n-1];
-// Due Tuesday, January 15, 2013 at 11:59 p.m. PST
 #include    <wb.h>
 
-#define BLOCK_SIZE 512 //@@ You can change this
 
-#define wbCheck(stmt) do {                                 \
-        cudaError_t err = stmt;                            \
-        if (err != cudaSuccess) {                          \
-            wbLog(ERROR, "Failed to run stmt ", #stmt);    \
-            return -1;                                     \
-        }                                                  \
+#define wbCheck(stmt) do {                                                    \
+        cudaError_t err = stmt;                                               \
+        if (err != cudaSuccess) {                                             \
+            wbLog(ERROR, "Failed to run stmt ", #stmt);                       \
+            wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err));    \
+            return -1;                                                        \
+        }                                                                     \
     } while(0)
 
-__global__ void total(float * input, float * output, int len) {
-    //@@ Load a segment of the input vector into shared memory
-    //@@ Traverse the reduction tree
-    //@@ Write the computed sum of the block to the output vector at the 
-    //@@ correct index
-}
+#define Mask_width  5
+#define Mask_radius Mask_width/2
 
-int main(int argc, char ** argv) {
-    int ii;
+//@@ INSERT CODE HERE
+
+
+int main(int argc, char* argv[]) {
     wbArg_t args;
-    float * hostInput; // The input 1D list
-    float * hostOutput; // The output list
-    float * deviceInput;
-    float * deviceOutput;
-    int numInputElements; // number of elements in the input list
-    int numOutputElements; // number of elements in the output list
+    int maskRows;
+    int maskColumns;
+    int imageChannels;
+    int imageWidth;
+    int imageHeight;
+    char * inputImageFile;
+    char * inputMaskFile;
+    wbImage_t inputImage;
+    wbImage_t outputImage;
+    float * hostInputImageData;
+    float * hostOutputImageData;
+    float * hostMaskData;
+    float * deviceInputImageData;
+    float * deviceOutputImageData;
+    float * deviceMaskData;
 
-    args = wbArg_read(argc, argv);
+    args = wbArg_read(argc, argv); /* parse the input arguments */
 
-    wbTime_start(Generic, "Importing data and creating memory on host");
-    hostInput = (float *) wbImport(wbArg_getInputFile(args, 0), &numInputElements);
+    inputImageFile = wbArg_getInputFile(args, 0);
+    inputMaskFile = wbArg_getInputFile(args, 1);
 
-    numOutputElements = numInputElements / (BLOCK_SIZE<<1);
-    if (numInputElements % (BLOCK_SIZE<<1)) {
-        numOutputElements++;
-    }
-    hostOutput = (float*) malloc(numOutputElements * sizeof(float));
+    inputImage = wbImport(inputImageFile);
+    hostMaskData = (float *) wbImport(inputMaskFile, &maskRows, &maskColumns);
 
-    wbTime_stop(Generic, "Importing data and creating memory on host");
+    assert(maskRows == 5); /* mask height is fixed to 5 in this mp */
+    assert(maskColumns == 5); /* mask width is fixed to 5 in this mp */
 
-    wbLog(TRACE, "The number of input elements in the input is ", numInputElements);
-    wbLog(TRACE, "The number of output elements in the input is ", numOutputElements);
+    imageWidth = wbImage_getWidth(inputImage);
+    imageHeight = wbImage_getHeight(inputImage);
+    imageChannels = wbImage_getChannels(inputImage);
 
-    wbTime_start(GPU, "Allocating GPU memory.");
-    //@@ Allocate GPU memory here
+    outputImage = wbImage_new(imageWidth, imageHeight, imageChannels);
 
-    wbTime_stop(GPU, "Allocating GPU memory.");
+    hostInputImageData = wbImage_getData(inputImage);
+    hostOutputImageData = wbImage_getData(outputImage);
 
-    wbTime_start(GPU, "Copying input memory to the GPU.");
-    //@@ Copy memory to the GPU here
+    wbTime_start(GPU, "Doing GPU Computation (memory + compute)");
 
-    wbTime_stop(GPU, "Copying input memory to the GPU.");
-    //@@ Initialize the grid and block dimensions here
+    wbTime_start(GPU, "Doing GPU memory allocation");
+    cudaMalloc((void **) &deviceInputImageData, imageWidth * imageHeight * imageChannels * sizeof(float));
+    cudaMalloc((void **) &deviceOutputImageData, imageWidth * imageHeight * imageChannels * sizeof(float));
+    cudaMalloc((void **) &deviceMaskData, maskRows * maskColumns * sizeof(float));
+    wbTime_stop(GPU, "Doing GPU memory allocation");
 
-    wbTime_start(Compute, "Performing CUDA computation");
-    //@@ Launch the GPU Kernel here
 
-    cudaDeviceSynchronize();
-    wbTime_stop(Compute, "Performing CUDA computation");
+    wbTime_start(Copy, "Copying data to the GPU");
+    cudaMemcpy(deviceInputImageData,
+               hostInputImageData,
+               imageWidth * imageHeight * imageChannels * sizeof(float),
+               cudaMemcpyHostToDevice);
+    cudaMemcpy(deviceMaskData,
+               hostMaskData,
+               maskRows * maskColumns * sizeof(float),
+               cudaMemcpyHostToDevice);
+    wbTime_stop(Copy, "Copying data to the GPU");
 
-    wbTime_start(Copy, "Copying output memory to the CPU");
-    //@@ Copy the GPU memory back to the CPU here
 
-    wbTime_stop(Copy, "Copying output memory to the CPU");
+    wbTime_start(Compute, "Doing the computation on the GPU");
+    //@@ INSERT CODE HERE
+    wbTime_stop(Compute, "Doing the computation on the GPU");
 
-    /********************************************************************
-     * Reduce output vector on the host
-     * NOTE: One could also perform the reduction of the output vector
-     * recursively and support any size input. For simplicity, we do not
-     * require that for this lab.
-     ********************************************************************/
-    for (ii = 1; ii < numOutputElements; ii++) {
-        hostOutput[0] += hostOutput[ii];
-    }
 
-    wbTime_start(GPU, "Freeing GPU Memory");
-    //@@ Free the GPU memory here
+    wbTime_start(Copy, "Copying data from the GPU");
+    cudaMemcpy(hostOutputImageData,
+               deviceOutputImageData,
+               imageWidth * imageHeight * imageChannels * sizeof(float),
+               cudaMemcpyDeviceToHost);
+    wbTime_stop(Copy, "Copying data from the GPU");
 
-    wbTime_stop(GPU, "Freeing GPU Memory");
+    wbTime_stop(GPU, "Doing GPU Computation (memory + compute)");
 
-    wbSolution(args, hostOutput, 1);
+    wbSolution(args, outputImage);
 
-    free(hostInput);
-    free(hostOutput);
+    cudaFree(deviceInputImageData);
+    cudaFree(deviceOutputImageData);
+    cudaFree(deviceMaskData);
+
+    free(hostMaskData);
+    wbImage_delete(outputImage);
+    wbImage_delete(inputImage);
 
     return 0;
 }
